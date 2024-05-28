27fe495e05ff5b3a7a1d68f2527ece53455c32e4
AnuOuseph <anuouseph04@gmail.com>
2024-05-28 23:13:00 +0530

    Design changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,13 +20,15 @@ function Home() {
             })
     }
   return (
-    <div className='md:px-20 md:py-20 px-10 py-32' id='home'>
-      <div className='flex grid grid-cols-1 md:grid-cols-2 items-center'>
-        <div className='bg-white flex md:justify-end justify-center'>
+    <div className='md:px-20 md:py-20 px-10 py-32 h-screen flex items-center justify-center' id='home'>
+      <div className='flex justify-center items-center'>
+        <div className='bg-white flex justify-center flex-col'>
             <div className=''>
-                <p className='md:text-6xl text-5xl py-4'><b>Mern Stack <br/> Developer</b></p>
-                <p className='text-sm py-2'>Hi, I'm Anu Ouseph, A passionate Full Stack Developer, <br/> based in Kerala, India.</p>
-                <div className='flex items-center'>
+                <p className='md:text-4xl text-2xl py-4 font-medium'>Hey, I'm Anu <span className="blog-title-emoji" style={{ transform: 'scaleX(-1)' }} >👋</span> <br /> <span className='md:text-5xl text-3xl '><b>Full Stack Developer</b></span></p>
+                
+                <p className='text-md py-1'>Hi, I'm Anu Ouseph, Experienced Full Stack Developer from India, <br/> with 2 years of expertise in React, Node, Express, MongoDb. </p>
+                {/* <div className='flex items-center'>
+                   // text gradient bg-gradient-to-r from-yellow-500 via-orange-600 to-red-600 inline-block text-transparent bg-clip-text
                     <a href="https://github.com/AnuOuseph" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon className='py-2 ' icon={['fab', 'github']} size="xl" />
                     </a>
@@ -36,42 +38,39 @@ function Home() {
                     
                     <button onClick={()=>{downloadFile(URL)}} className='btn'><FontAwesomeIcon className='py-2 px-2' icon={faDownload} size="lg" /></button>
                     <p className='text-sm font-medium'>Resume.</p>
-                    
-                </div>
+                     <img src="../../Picsart.png" alt="" /> 
+                </div> */}
+            </div>
+            <div className='md:flex items-center justify-start bg-white'>
+                <p className='md:px-0 py-2'><b className='text-sm md:text-md'>Tech Stack : </b></p>
+                <ul className='flex items-center'>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../mongo-icon.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../js.png" alt="" />
+                    </li>
+                    <li className='md:w-12 w-8 px-2'>
+                        <img src="../../node-icon.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../react-icon.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../html.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../css-icon.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../bs-icon.png" alt="" />
+                    </li>
+                    <li className='md:w-10 w-8 px-2'>
+                        <img src="../../tail-icon.png" alt="" />
+                    </li>
+                </ul>
             </div>
         </div>
-        <div className='bg-white'>
-            <img className='md:w-3/4' src="../../Picsart.png" alt="" />
-        </div>
-      </div>
-      <div className='flex items-center justify-center bg-white'>
-        <p className='md:px-2'><b className='text-sm md:text-md'>Tech Stack </b> | </p>
-        <ul className='flex items-center'>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../mongo-icon.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../js.png" alt="" />
-            </li>
-            <li className='md:w-12 w-8 px-2'>
-                <img src="../../node-icon.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../react-icon.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../html.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../css-icon.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../bs-icon.png" alt="" />
-            </li>
-            <li className='md:w-10 w-8 px-2'>
-                <img src="../../tail-icon.png" alt="" />
-            </li>
-        </ul>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,97 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-scroll'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faDownload, faXmark} from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const URL = "http://localhost:5173/Anu-Ouseph.pdf"
+    const downloadFile = (url)=>{
+      fetch(url).then(response=>response.blob())
+      .then((blob)=>{
+        const blobUrl = window.URL.createObjectURL(new Blob([blob]))
+        const filename = url.split('/').pop()
+        const aTag = document.createElement('a')
+        aTag.href = blobUrl
+        aTag.setAttribute('download',filename)
+        document.body.appendChild(aTag)
+        aTag.click()
+        aTag.remove()
+        })
+      }
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className='w-full fixed top-0 left-0 bg-white overflow-x-hidden flex justify-between py-4 px-10'>
-      <div>
-        <p className='md:text-lg text-sm font-semibold'>Anu Ouseph.</p>
+      <div className='flex items-center'>
+        <a href="https://github.com/AnuOuseph" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className='py-2 ' icon={['fab', 'github']}  size="md" />
+        </a>
+        <a href="https://www.linkedin.com/in/anuouseph/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className='py-2 px-4' icon={['fab', 'linkedin']}  size="md" /> 
+        </a>
+        <a href="https://www.linkedin.com/in/anuouseph/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className='py-2 px-0' icon={['fab', 'twitter']}  size="md" /> 
+        </a>
+        <button onClick={()=>{downloadFile(URL)}} className='btn'><FontAwesomeIcon className='py-2 px-4' icon={faDownload}  size="sm" /></button>
+        {/* <p className='text-sm font-medium'>Resume.</p>  */}
       </div>
-      <div>
-        <ul className='decoration-none flex font-medium'>
-          <li className='px-2 text-xs md:text-sm'><Link activeClass="active"
-                    to="home" // ID of the target component you want to scroll to
-                    spy={true}
-                    smooth={true}
-                    offset={-70} // Offset value to adjust the scroll position (optional)
-                    duration={500} // Duration of the scroll animation in milliseconds
-                  >
-                    <button>Home</button>
-                </Link>
-          </li>
-          <li className='px-2 text-xs md:text-sm'><Link activeClass="active"
-                    to="about" // ID of the target component you want to scroll to
-                    spy={true}
-                    smooth={true}
-                    offset={-70} // Offset value to adjust the scroll position (optional)
-                    duration={500} // Duration of the scroll animation in milliseconds
-                  >
-                    <button>About</button>
-                </Link>
-          </li>
-          <li className='px-2 text-xs md:text-sm'><Link activeClass="active"
-                    to="projects" // ID of the target component you want to scroll to
-                    spy={true}
-                    smooth={true}
-                    offset={-70} // Offset value to adjust the scroll position (optional)
-                    duration={500} // Duration of the scroll animation in milliseconds
-                  >
-                    <button>Projects</button>
-                </Link>
-          </li>
-          <li className='px-2 text-xs md:text-sm'><Link activeClass="active"
-                    to="contact" // ID of the target component you want to scroll to
-                    spy={true}
-                    smooth={true}
-                    offset={-70} // Offset value to adjust the scroll position (optional)
-                    duration={500} // Duration of the scroll animation in milliseconds
-                  >
-                    <button>Contact</button>
-                </Link>
-          </li>
-        </ul>
+      <button className="bg-white px-4 py-2 rounded" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      {isOpen && (
+        <div className="bg-white shadow-lg rounded md:w-[30%] w-[100%] min-h-screen bg-white fixed top-0 right-0 p-5" style={{ maxHeight: '100vh' }}>
+          <div className='text-end px-5'>
+            <FontAwesomeIcon icon={faXmark} className='cursor-pointer' onClick={toggleMenu}/>
+          </div>
+        <div className='px-5 bg-white flex items-center justify-center h-[90vh]'>
+          <ul className='decoration-none flex flex-col items-center font-medium px-5 py-4' >
+            <li className='px-4 py-4 text-xl md:text-3xl'><Link activeClass="active"
+              to="home" 
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500} 
+            >
+              <button>Home</button>
+          </Link>
+            </li>
+            <li className='px-4 py-4 text-xl md:text-3xl'><Link activeClass="active"
+              to="about" 
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500} 
+            >
+              <button>About</button>
+              </Link>
+            </li>
+            <li className='px-4 py-4 text-xl md:text-3xl'><Link activeClass="active"
+              to="projects" 
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500} 
+            >
+              <button>Projects</button>
+              </Link>
+            </li>
+            <li className='px-4 py-4 text-xl md:text-3xl'><Link activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70} 
+              duration={500} 
+            >
+              <button>Contact</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
+      )}
     </div>
   )
 }
